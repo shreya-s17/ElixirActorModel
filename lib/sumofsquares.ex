e8d6056dfffa2744d4f926a088f51f859637e702
Shreya <shreya171995@gmail.com>
2018-09-10 17:54:50 -0400

    Final project

--- lib/sumofsquares.ex
+++ lib/sumofsquares.ex
@@ -1,12 +1,12 @@
 defmodule SUMOFSQUARES do
   use GenServer
- 
+
   def start_link() do
     GenServer.start_link(__MODULE__ ,[])
   end
- 
+
   #---------------------- Callbacks ----------------------#
- 
+
   def handle_cast({:result,value},state) do
     {:noreply, [value | state]}
   end
@@ -17,37 +17,40 @@ defmodule SUMOFSQUARES do
     {:ok, args}
   end
   #------------------------ Client - side -----------------#
- 
+
   def main(n,k) do
     {:ok,pid}=SUMOFSQUARES.start_link
     spawnBulkProcesses(trunc(:math.pow(n,1/8)),n,k, pid,1)
-   IO.puts(" #{inspect :erlang.statistics(:runtime)}")
     IO.inspect(GenServer.call(pid,:result),limit: :infinity)
   end
- 
-  def spawnBulkProcesses(no,n,k,pid,i) do 
-    if(i <= n) do
-      spawn(fn -> send self(), bulkCalculate(i, checkValue(i,no,n),k,pid) end) 
-      spawnBulkProcesses(no,n,k,pid,i+no)
-    end
-  end 
- 
-  def checkValue(i,no,n) do
-    if(i+no>n) do 
+
+  def spawnBulkProcesses(no,n,k,pid,i) do
+    tasks =
+      for x <- i..trunc(Float.ceil(n/no)) do
+        Task.async(fn ->
+          bulkCalculate((x*no)-no+1, checkValue(x*no,n) ,k,pid)
+          x
+    end)
+  end
+  Enum.map(tasks, &Task.await/1)
+  end
+
+  def checkValue(xno, n) do
+    if(xno > n) do
       n
-    else 
-      i+no-1 
+    else
+      xno
     end
   end
   def bulkCalculate(start,n,k, pid) do
     Enum.map(start..n, fn(x)-> calculateSum(x,x+k-1,pid) end)
   end
- 
+
   def calculateSum(start, last, pid) do
     sum = Enum.sum(Enum.map(start..last, fn(x)->x*x end))
     sqrt = :math.sqrt(sum)
     if( sqrt == trunc(sqrt)) do
       GenServer.cast(pid,{:result, start})
     end
-    end
- end
+  end
+end
